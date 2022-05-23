0d4534e2fec7b2a28c92cd8e72f060ba3f093c75
김태완 <gimtaewan@gimtaewan-ui-MacBookAir.local>
2022-05-24 03:22:48 +0900

    zeroth commit

---
+++ C_basic_function/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_basic_function
+//
+//  Created by 김태완 on 2022/05/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
